af566f616d2a3ad3fe08cdf8ec27216fc3cf9709
nguyenthanhphat1510 <nguyenthanhphat2004.st@gmail.com>
2025-09-17 10:13:29 +0700

    fix configguration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,41 +2,36 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
-// import { UsersModule } from './users/users.module';
-// import { AuthModule } from './auth/auth.module';
 import { ProductsModule } from './products/products.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { CategoryModule } from './category/category.module';
 import { SubcategoryModule } from './subcategory/subcategory.module';
 import { CartModule } from './cart/cart.module';
-import { OrderModule } from './order/order.module'; 
+import { OrderModule } from './order/order.module';
 import { GeminiModule } from './gemini/gemini.module';
-import { PaymentModule } from './payment/payment.module'; // Import the Gemini module
-import { ReportsModule } from './reports/reports.module'; // Import the Reports module
+import { PaymentModule } from './payment/payment.module';
+import { ReportsModule } from './reports/reports.module';
 
-
-
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: '.env',
-    }),
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
     DatabaseModule,
     CartModule,
     GeminiModule,
-    // UsersModule,
-    // AuthModule,
     ProductsModule,
     AuthModule,
     UserModule,
     CategoryModule,
     SubcategoryModule,
-     OrderModule,
-     PaymentModule,
-     ReportsModule,
+    OrderModule,
+    PaymentModule,
+    ReportsModule,
   ],
+  controllers: [AppController],   // ✅ thêm
+  providers: [AppService],        // ✅ thêm
 })
-export class AppModule {}
+export class AppModule { }
