4ebb95185c77bf9130320ead451d82086c2da698
nguyenthanhphat1510 <nguyenthanhphat2004.st@gmail.com>
2025-09-17 14:08:13 +0700

    fix

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -5,10 +5,14 @@ import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import * as express from 'express';
 import { ConfigService } from '@nestjs/config';
+import { INestApplication } from '@nestjs/common';
 
 export async function createNestServer() {
     const server = express();
-    const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+    const app: INestApplication = await NestFactory.create(
+        AppModule, 
+        new ExpressAdapter(server)
+    );
 
     const config = app.get(ConfigService);
 
@@ -20,7 +24,7 @@ export async function createNestServer() {
 
     // ❗️ĐỪNG đặt global prefix 'api' trong Vercel
     // Vì tất cả request đã đi qua /api/index.ts. Nếu đặt nữa sẽ thành /api/api/...
-    if (!process.env.NODE_ENV) {
+    if (!process.env.VERCEL) {
         // chạy local mới cần prefix cho gọn
         app.setGlobalPrefix('api');
     }
